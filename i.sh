a2e8cbedc10a35c5f98436d4d8f439e3b7a74968
Stan-Di <147297339+Stan-Di@users.noreply.github.com>
2024-03-23 15:44:51 +0300

    Update i.sh

--- i.sh
+++ i.sh
@@ -9,6 +9,6 @@ curl -LO https://github.com/Stan-Di/nightscout-termux/releases/download/0.0.1/se
 dpkg -i nodejs-lts_18.16.1-2_aarch64.deb
 apt --fix-broken install -y
 tar xzf cgm-termux.tar.gz --checkpoint-action="ttyout=."
-rm cgm-termux.tar.gz
+rm cgm-termux.tar.gz nodejs-lts_18.16.1-2_aarch64.deb service.tar.gz
 tar xzf service.tar.gz -C $PREFIX/var/
 exit
