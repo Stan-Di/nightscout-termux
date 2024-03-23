794cfdbebb6e09ca8e9efd1ecf4730802c5aada3
Stan-Di <147297339+Stan-Di@users.noreply.github.com>
2024-03-23 15:34:13 +0300

    Update i.sh

--- i.sh
+++ i.sh
@@ -2,6 +2,7 @@
 pkg i termux-services -y
 pkg i tur-repo -y
 pkg update -y
+pkg i mongodb -y
 curl -LO https://github.com/Stan-Di/nightscout-termux/releases/download/0.0.1/nodejs-lts_18.16.1-2_aarch64.deb
 curl -LO https://github.com/Stan-Di/nightscout-termux/releases/download/0.0.1/cgm-termux.tar.gz
 curl -LO https://github.com/Stan-Di/nightscout-termux/releases/download/0.0.1/service.tar.gz
