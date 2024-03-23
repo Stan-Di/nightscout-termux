87cb0cf995c9f8e6d2a486842a9d807a6684b98d
Stan-Di <147297339+Stan-Di@users.noreply.github.com>
2024-03-23 14:55:11 +0300

    Update i.sh

--- i.sh
+++ i.sh
@@ -8,8 +8,7 @@ curl -O https://github.com/Stan-Di/nightscout-termux/releases/download/0.0.1/cgm
 curl -O https://github.com/Stan-Di/nightscout-termux/releases/download/0.0.1/service.tar.gz
 dpkg -i nodejs-lts_18.16.1-2_aarch64.deb
 apt --fix-broken install -y
-dpkg -i mongodb_6.1.0-1_aarch64.deb
 tar xzf cgm-termux.tar.gz --checkpoint-action="ttyout=."
-rm ns_termux.tar.gz
+rm cgm-termux.tar.gz
 tar xzf service.tar.gz -C $PREFIX/var/
 exit
