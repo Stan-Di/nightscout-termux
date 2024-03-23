d07565479ffdaf767e4d9ca8c201bde141af223e
Stan-Di <147297339+Stan-Di@users.noreply.github.com>
2024-03-23 15:06:58 +0300

    Update i.sh

--- i.sh
+++ i.sh
@@ -3,9 +3,9 @@ pkg update -y
 pkg i termux-services -y
 pkg i tur-repo -y
 pkg update -y
-curl -O https://github.com/Stan-Di/nightscout-termux/releases/download/0.0.1/nodejs-lts_18.16.1-2_aarch64.deb
-curl -O https://github.com/Stan-Di/nightscout-termux/releases/download/0.0.1/cgm-termux.tar.gz
-curl -O https://github.com/Stan-Di/nightscout-termux/releases/download/0.0.1/service.tar.gz
+curl -LO https://github.com/Stan-Di/nightscout-termux/releases/download/0.0.1/nodejs-lts_18.16.1-2_aarch64.deb
+curl -LO https://github.com/Stan-Di/nightscout-termux/releases/download/0.0.1/cgm-termux.tar.gz
+curl -LO https://github.com/Stan-Di/nightscout-termux/releases/download/0.0.1/service.tar.gz
 dpkg -i nodejs-lts_18.16.1-2_aarch64.deb
 apt --fix-broken install -y
 tar xzf cgm-termux.tar.gz --checkpoint-action="ttyout=."
