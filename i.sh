b87440dbdc1168c91aa025dcc4c58dd7c34db313
Stan-Di <147297339+Stan-Di@users.noreply.github.com>
2024-07-24 14:06:45 +0300

    Added libcrypt.gz

--- i.sh
+++ i.sh
@@ -1,5 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/sh
-pkg i termux-services libcrypt -y
+pkg i termux-services -y
 pkg i tur-repo -y
 pkg update -y
 pkg i mongodb -y
@@ -7,7 +7,9 @@ curl -LO https://github.com/Stan-Di/nightscout-termux/releases/download/0.0.1/no
 curl -LO https://github.com/Stan-Di/nightscout-termux/releases/download/0.0.1/cgm-termux.tar.gz
 curl -LO https://github.com/Stan-Di/nightscout-termux/releases/download/0.0.1/service.tar.gz
 curl -LO https://github.com/Stan-Di/nightscout-termux/releases/download/0.0.1/libicu.tar.gz
+curl -LO https://github.com/Stan-Di/nightscout-termux/releases/download/0.0.1/libcrypto.gz
 tar xzf libicu.tar.gz -C $PREFIX/lib/
+tar xzf libcrypto.gz -C $PREFIX/lib/
 dpkg -i nodejs-lts_18.16.1-2_aarch64.deb
 apt --fix-broken install -y
 tar xzf cgm-termux.tar.gz --checkpoint-action="ttyout=."
